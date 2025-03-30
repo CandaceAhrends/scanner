8193c417984b623c7cfb26c7041ad70d0cc1e432
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2025-03-30 12:38:25 -0400

    saving redis stream reader

--- reader.mjs
+++ reader.mjs
@@ -9,6 +9,10 @@ const redisKeys = PARTITION_KEYS.map((key) => `stocks-${key}`);
 const getScannerData = async () => {
   await redis.connect();
   console.log("🔌 Connected to Redis");
+
+  const result = await redis.xRead([{ key: redisKeys[0], id: "0" }], {
+    count: 100,
+  });
 };
 
 const test = getScannerData();
