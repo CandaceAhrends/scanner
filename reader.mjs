d8ae72938f88136e64ee45e3b9ed3d3af4be596d
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2025-03-30 13:10:39 -0400

    saving redis stream reader

--- reader.mjs
+++ reader.mjs
@@ -12,21 +12,26 @@ const redisKeys = PARTITION_KEYS.map((key) => ({
 const getScannerData = async () => {
   await redis.connect();
   console.log("🔌 Connected to Redis", redisKeys);
-
-  const result = await redis.xRead(redisKeys, {
-    count: 100,
-    block: 0,
-  });
-  console.log("📚 Data from Redis", result);
-
-  result.forEach((data) => {
-    data.messages.forEach((msg) => {
-      const gainerMessage = msg?.message?.topGainers;
-      if (!gainerMessage) return;
-      const gaienrs = JSON.parse(gainerMessage);
-      console.log(gaienrs);
+  try {
+    const result = await redis.xRead(redisKeys, {
+      count: 100,
+      block: 0,
     });
-  });
-};
+    console.log("📚 Data from Redis", result);
 
-const test = getScannerData();
+    result.forEach((data) => {
+      data.messages.forEach((msg) => {
+        const gainerMessage = msg?.message?.topGainers;
+        if (!gainerMessage) return;
+        const gaienrs = JSON.parse(gainerMessage);
+        console.log(gaienrs);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+    await new Promise((res) => setTimeout(res, 1000));
+  }
+};
+while (true) {
+  getScannerData();
+}
